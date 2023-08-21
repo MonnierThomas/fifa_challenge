d224a135431f3835a5943badef3fde54848d9812
Thomas Monnier <54896884+MonnierThomas@users.noreply.github.com>
2023-08-21 16:43:50 +0200

    start

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+
+
+st.set_page_config(
+    page_title="FIFA",
+    page_icon="👋",
+    layout="wide"
+)
+
+st.markdown(
+        "<h1 style='text-align: center; color: blue;'>Fifa Streamlit App</h1>",
+        unsafe_allow_html=True,
+    )
+
+def main():
+    with open("database.csv", "r") as file:
+        st.session_state.df = pd.read_csv(file)
+
+    edited_df = st.data_editor(
+        st.session_state.df,
+        num_rows="dynamic",
+    )
+
+    if not edited_df.equals(st.session_state.df):
+        edited_df.to_csv("database.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
